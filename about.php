26f217f53ece0874123eddfd61faafd799834f34
Yogi Wahyu <yogiwahyu48@gmail.com>
2019-01-31 11:19:24 +0700

    Add files via upload
    Make with CSS3, HTML5, Bootstrap, Js, dan SCSS...

---
+++ about.php
@@ -0,0 +1,200 @@
+<?php 
+
+echo "";
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Linda | About</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style type="text/css">
+		#add_img{
+			border-radius: 21px;
+		}
+	</style>
+
+
+</head>
+<body>
+
+	<!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        <!-- Navbar Area -->
+        <div class="credit-main-menu" id="sticker">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="creditNav">
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+                        <!-- Menu -->
+                        <div class="classy-menu">
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <!-- <li><a href="about.html">About Us</a></li> -->
+                                    <!-- <li><a href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="about.html">About Us</a></li>
+                                            <li><a href="services.html">Services</a></li>
+                                            <li><a href="post.html">Post</a></li>
+                                            <li><a href="single-post.html">Single Post</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="elements.html">Elements</a></li>
+                                        </ul>
+                                    </li> -->
+                                    <li><a href="#">Insight</a></li>
+                                    <!-- <li><a href="services.html">Services</a></li> -->
+                                    <li><a href="#">FAQ</a></li>
+                                    <!-- <li class="btn btn-outline-primary"><a href="#">Login</a></li>
+                                    <li class="btn btn-outline-primary"><a href="#">Create Account</a></li> -->
+                                    <!-- <li><a href="#">Portfolio</a></li> -->
+                                    <!-- <li><a href="post.html">Blog</a></li> -->
+                                    <!-- <li><a href="contact.html">Contact</a></li> -->
+                                </ul>
+                                <ul>
+                                    <li class="btn btn-outline-primary"><a href="#">Login</a></li>
+                                    <li class="btn btn-outline-primary"><a href="#">Create Account</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                        <!-- Contact -->
+                        <div class="contact">
+                            <a href="#"><img src="img/Logo-Lubna.png" alt=""></a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- end header -->
+    <!-- body start ### section -->
+    <section class="breadcrumb-area bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/13.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="breadcrumb-content">
+                        <h2>About us</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">About Us</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end section -->
+    <!-- start about -->
+    <section class="about-area section-padding-100-0">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6">
+                    <div class="about-content mb-100">
+                        <!-- Section Heading -->
+                        <div class="section-heading">
+                            <div class="line"></div>
+                            <p>Take look at our</p>
+                            <h2>About Our Company</h2>
+                        </div>
+                        <h6 class="mb-4">In vitae nisi aliquam, scelerisque leo a, volutpat sem. Viva mus rutrum dui fermentum eros hendrerit, id lobortis leo volutpat. </h6>
+                        <p class="mb-0">Sed ut iaculis elit, quis varius mauris. Integer ut ultricies orci, lobortis egestas sem. Duis non volutpat arcu. Sed ut iaculis elit, quis varius mauris. Integer ut ultricies orci, lobortis egestas sem. Duis non volutpat arcu. Pellentesque lobortis neque non sem dapibus, non rutrum dolor pretium.</p>
+                        <a href="#" class="btn credit-btn mt-50">Discover</a>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="about-thumbnail mb-100">
+                        <img src="img/lubna_members.jpeg" alt="" style="border-radius: 21px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end about -->
+    <!--  Call To Action Start -->
+    <section class="cta-area d-flex flex-wrap">
+        <!-- Cta Content -->
+        <div class="cta-content">
+            <!-- Section Heading -->
+            <div class="section-heading white">
+                <div class="line"></div>
+                <p>Bold desing and beyound</p>
+                <h2>Some Milestones</h2>
+            </div>
+            <h6 class="mb-0">Morbi ut dapibus dui. Sed ut iaculis elit, quis varius mauris. Integer ut ultricies orci, lobortis egestas sem. Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sod ales. Maecenas sed magna tempor, efficitur maur is in, sollicitudin augue.</h6>
+
+            <div class="d-flex flex-wrap align-items-center justify-content-between">
+                <!-- Single Cool Facts -->
+                <div class="single-cool-fact white d-flex align-items-center mt-50">
+                    <div class="scf-icon mr-15">
+                        <i class="icon-piggy-bank"></i>
+                    </div>
+                    <div class="scf-text">
+                        <h2><span class="counter">8710</span></h2>
+                        <p>Clients</p>
+                    </div>
+                </div>
+                <!-- Single Cool Facts -->
+                <div class="single-cool-fact white d-flex align-items-center mt-50">
+                    <div class="scf-icon mr-15">
+                        <i class="icon-coin"></i>
+                    </div>
+                    <div class="scf-text">
+                        <h2><span class="counter">35</span></h2>
+                        <p>Creditors</p>
+                    </div>
+                </div>
+                <!-- Single Cool Facts -->
+                <div class="single-cool-fact white d-flex align-items-center mt-50">
+                    <div class="scf-icon mr-15">
+                        <i class="icon-diamond"></i>
+                    </div>
+                    <div class="scf-text">
+                        <h2><span class="counter">12</span></h2>
+                        <p>awards</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Cta Thumbnail -->
+        <div class="cta-thumbnail bg-img jarallax" style="background-image: url(img/backtesting.png);"></div>
+    </section>
+    <!-- end action tag -->
+
+
+
+    <!-- ##### Footer Area Start ##### -->
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <!-- <script src="js/bootstrap/popper.min.js"></script> -->
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+
+
+    <!-- addthis.com -->
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c52148b0531f037"></script>
+</body>
+</html>
